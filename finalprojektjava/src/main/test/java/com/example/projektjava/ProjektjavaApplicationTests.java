9c015165826c21cee88cdf20cb78aa9f047debbd
Natalia020 <59650296+Natalia020@users.noreply.github.com>
2022-02-21 16:16:50 +0100

    Add files via upload

---
+++ finalprojektjava/src/main/test/java/com/example/projektjava/ProjektjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projektjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
